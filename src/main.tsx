05241cf8a1e465962da2f7966d99aefcfc671c1a
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-07 11:55:24 -0500

    productDetails

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from "./Pages/Home.tsx";
 import AddProduct from "./Pages/AddProduct.tsx";
 import EditProduct from "./Pages/EditProduct.tsx";
-import Home from "./Pages/Home.tsx";
 import { Layout } from "./components/Layout.tsx";
 import Favorites from "./Pages/Favorites.tsx";
+import ProductDetails from "./Pages/ProductDetails.tsx";
 
 const router = createBrowserRouter([
   {
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
       {
         path: "/Favorites",
         element: <Favorites />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails />,
       },
     ],
   },
