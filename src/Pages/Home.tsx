b9754df19c73a67c4561462a0bf761680a8908b4
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-07 12:53:48 -0500

    change

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,10 @@
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 const Home = () => {
   return <div>Home</div>;
   <Navbar />;
+  <Header />;
 };
 
 export default Home;
