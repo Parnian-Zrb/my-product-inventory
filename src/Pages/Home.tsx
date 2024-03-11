0a4187b0c452ed99b274b41f2a6dc780f49d1d21
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-11 10:23:00 -0400

    Json update

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,19 @@
-import ProfileCard from "../components/ProductCard";
+import ProductCard from "../components/ProductCard";
+import products from "../data/products.json";
+
 const Home = () => {
+  const productsArray = products.products.data.items;
   return (
     <div>
-      <ProfileCard />
-      <ProfileCard />
+      {productsArray.map((product) => (
+        <ProductCard
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          rating={4.5}
+          image={""}
+        />
+      ))}
     </div>
   );
 };
