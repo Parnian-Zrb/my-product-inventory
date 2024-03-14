b53b4c7a14e0be553bbc1601112413f49046915f
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-14 12:23:41 -0400

    update

--- src/Pages/ProductDetails.tsx
+++ src/Pages/ProductDetails.tsx
@@ -2,28 +2,49 @@ import { useState, useEffect } from "react";
 import { ProductType } from "../types/products";
 
 import { useParams } from "react-router-dom";
+import { getProductById } from "../api/productApi";
 
 const ProductDetails = () => {
   const { id = "" } = useParams();
   const [product, setProduct] = useState<ProductType>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/product/${id}`)
-      .then((response) => response.json())
-      .then((data) => setProduct(data));
+    const fetchProduct = async () => {
+      try {
+        setIsLoading(true);
+        const responseData = await getProductById(id);
+        setProduct(responseData.data);
+      } catch (error) {
+        console.log("Error: ", error);
+      } finally {
+        setIsLoading(false);
+      }
+      //   const  response = await getProductById(id);
+      //   setProduct(response.data);
+      // } catch (error) {
+      //     console.log("Error: ", error);
+      // }finally{
+      //   setIsLoading(false)
+      // }
+    };
+    fetchProduct();
   }, [id]);
 
-  if (!product) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        <h1>{product.title}</h1>
-        <p>{product.description}</p>
-        <button> Update Product</button>
-        <button> Delete Product</button>
-      </div>
-    );
-  }
+  return (
+    <>
+      {product ? (
+        <>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <button> Update Product</button>
+          <button> Delete Product</button>
+        </>
+      ) : (
+        <p>Loading product details...</p>
+      )}
+    </>
+  );
 };
 
 export default ProductDetails;
