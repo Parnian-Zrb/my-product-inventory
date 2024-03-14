07d28b0d47b10fd50d5d22fcd9c9db4edc923443
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-14 12:03:04 -0400

    loading

--- src/Pages/ProductDetails.tsx
+++ src/Pages/ProductDetails.tsx
@@ -1,24 +1,29 @@
-// import { useEffect } from 'react';
-// import axios from 'axios';
-// import { updateProduct } from '../api/productApi';
+import { useState, useEffect } from "react";
+import { ProductType } from "../types/products";
 
-// interface ProductProps {
-//  title: string;
-//  price: string;
-//  description: string;
-//  category: string;
-//  rating: string;
-// }
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  return (
-    <div>
-      <h1>{ProductDetails.title}</h1>
-      <p>{ProductDetails.description}</p>
-      <button> Update Product</button>
-      <button> Delete Product</button>
-    </div>
-  );
+  const { id = "" } = useParams();
+  const [product, setProduct] = useState<ProductType>();
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/product/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, [id]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+        <button> Update Product</button>
+        <button> Delete Product</button>
+      </div>
+    );
+  }
 };
 
 export default ProductDetails;
