96efb650a122089d510abf349b14fe2175c83508
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-18 12:08:00 -0400

    undo

--- src/routes/userRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllUsers } from "../controllers/userController";
-const router = express.Router();
-
-router.route("/").get(getAllUsers);
-
-export default router;
