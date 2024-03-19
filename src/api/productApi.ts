acf718e4d4d444dcc684a0de89471bf7af923a5c
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-19 10:28:08 -0400

    add url undo

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -2,36 +2,29 @@
 //GET/POST/PATCH,PUT/DELETE
 
 import axios from "axios";
-const URL= "https://fakestoreapi.com/products"
+const URL = "https://fakestoreapi.com/products";
 
-//fetch all the  products 
-export const getAlldata =() => 
-axios.get(URL);
+//fetch all the  products
+export const getAlldata = () => axios.get(URL);
 
-interface ProductType{
-    title: string;
-    price: string;
-      description: string,
-      category: string,
-      rating: string,
-
-
+interface ProductType {
+  title: string;
+  price: string;
+  description: string;
+  category: string;
+  rating: string;
 }
 
 //create a new product
 export const createProduct = (productData: ProductType) =>
   axios.post(URL, productData);
 
-
 //get a single product
-export  const getProductById = (id :string)=>
-   axios.get(`${URL}/${id}`);
-
+export const getProductById = (id: string) => axios.get(`${URL}/${id}`);
 
 //update  an existing product
-export const updateProduct =  ( id: string, productData : ProductType) =>
-axios.patch(`${URL}/${id}`, productData );
+export const updateProduct = (id: string, productData: ProductType) =>
+  axios.patch(`${URL}/${id}`, productData);
 
-//delete  a product 
-export const deleteProduct = (id: string )=>
-     axios.delete (`${URL}/${id}`);
+//delete  a product
+export const deleteProduct = (id: string) => axios.delete(`${URL}/${id}`);
